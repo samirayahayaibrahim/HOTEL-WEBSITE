7c65bc6e2d560c000e68b117d17b71792e024fec
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2024-11-17 04:56:59 -0800

    render the whychooseus-section on js file and update the styling

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+const chooses = [
+        { image: "assets/img.png", title: "Payment methods", description: "We have a lot of them, from cryptocurrencies to barter for potatoes" },
+        { image: "assets/img (1).png", title: "Simple search process", description: "We checked it out, even the kid did it,<br> but it was my mom's friend's son" },
+        { image: "assets/img (2).png", title: "24/7 Support", description: "Is there something you don't <br>understand? Feel free to call us. <br>Phone number in the footer" },
+        { image: "assets/img (3).png", title: "We are nice", description: "Fantasy is over, there will be something really convincing here" },
+    ];
+
+    const renderChooses = chooses => {
+        const container = document.getElementById("chooseContainer");
+        chooses.forEach(choose => {
+            container.innerHTML += `
+                <div>
+                    <img class="image" src="${choose.image}" alt="${choose.image}">
+                    <h3 class="title">${choose.title}</h3>
+                    <p class="description">${choose.description}</p>
+                </div>
+            `;
+        });
+    }
+    
+    renderChooses(chooses);
